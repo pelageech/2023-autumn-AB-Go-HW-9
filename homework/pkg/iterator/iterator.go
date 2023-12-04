9885cd17349ee10c64dd5a5589e21f1542e0babe
pelageech <va.oleg2015@yandex.ru>
2023-12-04 22:12:32 +0700

    go mod go sum updated

--- homework/pkg/iterator/iterator.go
+++ homework/pkg/iterator/iterator.go
@@ -1,9 +1,9 @@
 package iterator
 
-// Interface defines a default iterator. It is used for
+// Simple defines a default iterator. It is used for
 // Iterate function. You should use this function for iterating.
-type Interface[T any] interface {
-	// HasNext defines if the next iteration exist.
+type Simple[T any] interface {
+	// HasNext defines if the next iteration exists.
 	HasNext() bool
 
 	// Next returns the next thing in iteration or error
@@ -11,10 +11,10 @@ type Interface[T any] interface {
 	Next() (T, error)
 }
 
-// Iterate iterates over Interface implementation and calls f
+// Iterate iterates over Simple implementation and calls f
 // on each iteration. If i.Next() of f returns error, the iteration
-// stops and the error that is returned by functions above is returned.
-func Iterate[T any](i Interface[T], f func(t T) error) error {
+// stops and the error returned by the functions above is returned.
+func Iterate[T any](i Simple[T], f func(t T) error) error {
 	if !i.HasNext() {
 		return nil
 	}
