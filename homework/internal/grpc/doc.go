c38fe943ee8bd5ade037407f274bcc4db4ffd203
pelageech <va.oleg2015@yandex.ru>
2023-12-11 16:08:56 +0700

    added doc.go for packages

---
+++ homework/internal/grpc/doc.go
@@ -0,0 +1,6 @@
+// Package grpc provides general GRPC structure
+// like server, client and interceptors for using
+// them in production.
+//
+// Their configuration is provided with config package.
+package grpc
